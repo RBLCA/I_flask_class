9b336ae53119fb4811807df38fb0a566b55a8917
Rohit Bansod <iafmigrohit4@gmail.com>
2023-08-29 22:22:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/' )
+def welcome():
+    return 'Welcome to the the Flask class.'
+
+@app.route('/calculator', methods = ['GET', 'POST'])
+def math_operation():
+    operation = request.json["operation"]
+    num1 = request.json['num1']
+    num2 = request.json['num2']
+
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'multiply':
+        result = num1 * num2 
+
+    elif operation == 'division':
+        result = num1 / num2
+    else:
+        result = num1 - num2
+
+    return result
+
+
+
+
+
+
+print(__name__)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
